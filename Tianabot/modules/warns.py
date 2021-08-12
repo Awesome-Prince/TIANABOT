0597efff03db9f25aab358a3303ed497b2818a9b
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-08-12 08:09:19 +0530

    Update warns.py

--- Tianabot/modules/warns.py
+++ Tianabot/modules/warns.py
@@ -122,6 +122,9 @@ def warn(
                 [
                     InlineKeyboardButton(
                         "🔘 Remove warn", callback_data="rm_warn({})".format(user.id)
+                    ),
+                    InlineKeyboardButton(
+                        "🆑 Chat Rules", url="https://t.me/PATRICIA_ROBOT?start={}".format(chat.id)
                     )
                 ]
             ]
@@ -129,7 +132,7 @@ def warn(
 
         reply = (
             f"<code>❕</code><b>Warn Event</b>\n"
-            f"<code> </code><b>•  User:</b> {mention_html(user.id, user.first_name)}\n"
+            f"<code> </code><b>• Stupid User:</b> {mention_html(user.id, user.first_name)}\n"
             f"<code> </code><b>•  Count:</b> {num_warns}/{limit}"
         )
         if reason:
